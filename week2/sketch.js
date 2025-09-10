4d06cc7c530af5b920abb6e5f3889a9666a08420
scarlwang <mw4016@nyu.edu>
2025-09-10 16:08:58 -0400

    Updated week2 sketch.js
    3 shapes

--- week2/sketch.js
+++ week2/sketch.js
@@ -2,9 +2,21 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("black");
   
 }
 
 function draw() {
-  circle(100,100,100);
+
+  noStroke();
+
+  fill("white");
+  rect(100,100,100,100);
+
+  fill("red");
+  circle(windowWidth/2,windowHeight/2,windowHeight/2);
+
+  fill("yellow");
+  triangle(200,200,windowWidth/2,windowHeight/2,500,500);
+
 }
